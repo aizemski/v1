763af385ec889266d001401b835e2fff7deeec49
ardize <aleksanderizemski@gmail.com>
2020-06-12 19:06:49 +0200

    Added bootstrap links and linked social media logos

--- src/shared/components/layout/Footer/index.js
+++ src/shared/components/layout/Footer/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Row, Image } from 'react-bootstrap';
+import { Row, Image, Col, Container } from 'react-bootstrap';
 import InstagramLogo from '../../../img/iconmonstr-instagram-11.svg';
 import GithubLogo from '../../../img/iconmonstr-github-1.svg';
 import LinkedinLogo from '../../../img/iconmonstr-linkedin-1.svg';
+
 const Footer = () => {
     return (
-        <Row>
-            <Image src={GithubLogo} />
-            <Image src={LinkedinLogo} />
-            <Image src={InstagramLogo} />
-        </Row>
+        <Container>
+            <Row className='justify-content-md-center' md='6'>
+                <Col md='auto'>
+                    <a href='https://github.com/aizemski'>
+                        <Image src={GithubLogo} />
+                    </a>
+                </Col>
+                <Col md='auto'>
+                    <a href='https://www.linkedin.com/in/aizemski/'>
+                        <Image src={LinkedinLogo} />
+                    </a>
+                </Col>
+                <Col md='auto'>
+                    <a href='https://www.instagram.com/aizemski/'>
+                        <Image src={InstagramLogo} />
+                    </a>
+                </Col>
+            </Row>
+            <Row className='justify-content-md-center' md='6'>
+                ©Aleksander Iżemski 2020
+            </Row>
+        </Container>
     );
 };
 export default Footer;
