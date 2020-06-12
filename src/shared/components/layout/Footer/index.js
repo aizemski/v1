6c9ddf06384a1769a522a29f4d91b9e8290429e0
ardize <aleksanderizemski@gmail.com>
2020-06-12 18:22:00 +0200

    Added social media logos

--- src/shared/components/layout/Footer/index.js
+++ src/shared/components/layout/Footer/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
-
+import { Row, Image } from 'react-bootstrap';
+import InstagramLogo from '../../../img/iconmonstr-instagram-11.svg';
+import GithubLogo from '../../../img/iconmonstr-github-1.svg';
+import LinkedinLogo from '../../../img/iconmonstr-linkedin-1.svg';
 const Footer = () => {
-    return <Row>footer</Row>;
+    return (
+        <Row>
+            <Image src={GithubLogo} />
+            <Image src={LinkedinLogo} />
+            <Image src={InstagramLogo} />
+        </Row>
+    );
 };
 export default Footer;
