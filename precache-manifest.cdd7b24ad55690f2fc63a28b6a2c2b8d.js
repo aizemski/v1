7fd516042bc54379b11bab4e142bbc4c05a9ec49
ardize <aleksanderizemski@gmail.com>
2020-06-12 15:24:06 +0200

    Updates

---
+++ precache-manifest.cdd7b24ad55690f2fc63a28b6a2c2b8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1de033f9fb9c0d21d4d9bcbb76a861",
+    "url": "/v1/index.html"
+  },
+  {
+    "revision": "89fcbe94cdd89ff68903",
+    "url": "/v1/static/css/main.65a6af82.chunk.css"
+  },
+  {
+    "revision": "01d8d1147177e19c601e",
+    "url": "/v1/static/js/2.bf642464.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/v1/static/js/2.bf642464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fcbe94cdd89ff68903",
+    "url": "/v1/static/js/main.ace03cc2.chunk.js"
+  },
+  {
+    "revision": "9f44a19df660421c78c2",
+    "url": "/v1/static/js/runtime-main.eea0bfc7.js"
+  }
+]);
