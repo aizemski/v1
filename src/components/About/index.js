396e7a6cedb8fc370416b2638139a56934b9dc81
ardize <aleksanderizemski@gmail.com>
2020-06-14 23:57:34 +0200

    Added photo and some dummy data for About.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
-
+import { Row, Col, Image } from 'react-bootstrap';
+import Me from '../../shared/img/IżemskiAleksander.jpg';
 const About = () => {
-    return <Row id='about'>About</Row>;
+    return (
+        <>
+            <Row id='about' className='justify-content-center'>
+                <h1>Few words about me</h1>
+            </Row>
+            <Row className='justify-content-center'>
+                <Image
+                    src={Me}
+                    roundedCircle
+                    width='256px'
+                    height='256px'
+                ></Image>
+            </Row>
+            <Row>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                </p>
+            </Row>
+        </>
+    );
 };
 export default About;
