d511529abfc5d28a0ae05d9f960be41273151860
ardize <aleksanderizemski@gmail.com>
2020-06-19 16:17:33 +0200

    OnScroll effects for header

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
-
+import '../../shared/css/contact.css';
 const Contact = () => {
-    return <Row id='contact'>Contact</Row>;
+    return (
+        <Row id='contact'>
+            {' '}
+            <p className='about-txt'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+            </p>
+        </Row>
+    );
 };
 export default Contact;
--- src/shared/components/layout/Header/index.js
+++ src/shared/components/layout/Header/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+
 import '../../../css/header.css';
 const Header = () => {
+    const navs = document.getElementsByClassName('nav-link');
+
+    window.addEventListener('scroll', () => {
+        const about = document.getElementById('about').getBoundingClientRect();
+        const work = document.getElementById('work').getBoundingClientRect();
+        const experience = document
+            .getElementById('experience')
+            .getBoundingClientRect();
+        const contact = document
+            .getElementById('contact')
+            .getBoundingClientRect();
+        const h = window.innerHeight;
+        const d = 4;
+        console.log(h);
+        navs[0].classList.remove('header-active');
+        navs[1].classList.remove('header-active');
+        navs[2].classList.remove('header-active');
+        navs[3].classList.remove('header-active');
+        if (h / d >= contact.top) {
+            navs[3].classList.add('header-active');
+        } else if (h / d >= experience.top) {
+            navs[2].classList.add('header-active');
+        } else if (h / d >= work.top) {
+            navs[1].classList.add('header-active');
+        } else if (h / d >= about.top) {
+            navs[0].classList.add('header-active');
+        }
+    });
+
     return (
         <Navbar expand='md' className='header'>
             <Navbar.Brand href='/'>
@@ -13,16 +42,16 @@ const Header = () => {
                 id='basic-navbar-nav'
                 className='justify-content-end'
             >
-                <Nav.Link href='#about' className='header-nav'>
+                <Nav.Link href='#about' className={'header-nav '}>
                     About
                 </Nav.Link>
-                <Nav.Link href='#work' className='header-nav'>
+                <Nav.Link href='#work' className={'header-nav '}>
                     Work
                 </Nav.Link>
-                <Nav.Link href='#experience' className='header-nav'>
+                <Nav.Link href='#experience' className={'header-nav '}>
                     Experience
                 </Nav.Link>
-                <Nav.Link href='#contact' className='header-nav'>
+                <Nav.Link href='#contact' className={'header-nav '}>
                     Contact
                 </Nav.Link>
             </Navbar.Collapse>
