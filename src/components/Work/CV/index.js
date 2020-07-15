8387f99aead8e0be88edc546808e13ba0c16d804
ardize <aleksanderizemski@gmail.com>
2020-07-15 11:50:39 +0200

    Added CV

--- src/components/Work/CV/index.js
+++ src/components/Work/CV/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Row ,Col,Button} from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 
 const CV = (props) => {
     {
         /*TODO: Grab CV - Polish and English version*/
     }
-    return <Row className='cv-container'><Col><h3>CHECK OUT MY RÉSUMÉ!</h3>
-    <Button className='cv-grab'>Grab A Copy</Button></Col></Row>;
+
+    return (
+        <Row className='cv-container'>
+            <Col>
+                <h3>CHECK OUT MY RÉSUMÉ!</h3>
+                <Button
+                    className='cv-grab'
+                    href='v1/src/shared/other/resume2020.pdf'
+                    target='_blank'
+                >
+                    Grab A Copy
+                </Button>
+            </Col>
+        </Row>
+    );
 };
 export default CV;
