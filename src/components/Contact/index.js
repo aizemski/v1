d25ff48caa51e883391bac04e8a81740f443ef27
ardize <aleksanderizemski@gmail.com>
2020-07-15 18:16:25 +0200

    Finished Contact section, changed borders width for buttons, changed footer layout

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Row,Col,Button,Image } from 'react-bootstrap';
+import { Row, Col, Button, Image } from 'react-bootstrap';
 import '../../shared/css/contact.css';
-import Mail from '../../shared/img/mail.svg'
+import Mail from '../../shared/img/mail.svg';
 const Contact = () => {
-    const size = '64px';
     return (
-        <Row id='contact'>
-           
-            <Col>
-{/* Icons made by https://www.flaticon.com/authors/itim2101*/}
-            <Image src={Mail} width={size}/>
-            <h2 >
-            GET IN TOUCH!
-            </h2>
-            <Button>Say Hello </Button></Col>
+        <Row id='contact' className='justify-content-center'>
+            <Col className='contact-container d-flex flex-column align-items-center '>
+                {/* Icons made by https://www.flaticon.com/authors/itim2101*/}
+                <Image src={Mail} />
+                <h2>GET IN TOUCH!</h2>
+                <p>
+                    {' '}
+                    In case you have an idea for a project or just want to chat,
+                    feel free to shoot me an email!
+                </p>
+                <Button
+                    className='contact-button'
+                    href='mailto:aleksanderizemski@gmail.com'
+                >
+                    Say Hello{' '}
+                </Button>
+            </Col>
         </Row>
     );
 };
